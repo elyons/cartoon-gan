186f8ff017f2b09d3b9db2416800556269cc6b35
Eric Lyons <lyonsden@gmail.com>
2023-03-15 08:31:44 -0700

    inital import (migrated from notebook)

---
+++ create_smooth_dataset.py
@@ -0,0 +1,3 @@
+from utils import datasets
+# Specify the folder with images you want to use for creating a smoothed dataset.
+datasets.edge_promoting("./datasets/cartoon_images/Akira", "./datasets/cartoon_images_smoothed/Akira")
